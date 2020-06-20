19f446022c41f6bdd82a1997e970c0a56e54268b
Yash Kapoor <kapooryash713@gmail.com>
2020-06-20 19:13:39 +0530

    Form Validation

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,5 +1,8 @@
 import React,{Component} from 'react';
-import {Button, Form,Label,Input,Col, FormGroup } from 'reactstrap';
+import {Button, Form,Label,Input,Col, FormGroup, FormFeedback } from 'reactstrap';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Contact extends Component {
 
@@ -12,11 +15,84 @@ class Contact extends Component {
             Description: '',
             Eventlink: '',
             agree: false,
-            Contact: 'Tel'
+            Contact: 'Tel',
+            startDate: new Date(),
+            touched: {
+                Eventname: false,
+                Location: false,
+                Description: false,
+                Contact: false
+            }
 
         };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
     }
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        
+    
+        this.setState({
+          [name]: value
+        });
+        
+    }
+
+    handleSubmit(event) {
+        console.log('Current State is: ' + JSON.stringify(this.state));
+        alert('Current State is: ' + JSON.stringify(this.state));
+        event.preventDefault();
+    }
+    
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    })
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.startDate)
+  }
+
+  handleBlur=(field)=>(evt)=>{
+      this.setState({
+          touched: {...this.state.touched, [field]:true}
+      });
+  }
+  validate(Eventname,Location,Description,Eventlink){
+      const errors={
+        Eventname: '',
+        Location: '',
+        Description: '',
+        Contact: ''
+      };
+
+      if(this.state.touched.Eventname && Eventname.length<2)
+      errors.Eventname = 'Eventname should be greater than 1 character.';
+      
+      if(this.state.touched.Description && Description.length<3)
+      errors.Description= 'Description should be valid';
+      else if(this.state.touched.Description && Description.length>100)
+      errors.Description='Description should be under range';
+
+      const reg= /^\d+$/;
+      if(this.state.touched.Contact && !reg.test(Contact))
+      errors.Contact='Contact number should contain only numbers';
+
+
+      return errors;
+
+
+  }
     render(){
+
+        const errors=this.validate(this.state.Eventname, this.state.Description, this.state.Contact);
+        
 
         return(
             <div className="container">
@@ -31,14 +107,18 @@ class Contact extends Component {
                 <div className="row row-content">
                     
                     <div className="col-12 col-md-9">
-                        <Form>
+                        <Form onSubmit={this.handleSubmit}>
                             <FormGroup row>
                             <Label htmlFor="Eventname" md={2}>Event Name</Label>
                                 <Col md={10}>
                                     <Input type="text" id="Eventname" name="Eventname"
                                         placeholder="Event Name"
                                         value={this.state.Eventname}
+                                        valid={errors.Eventname === ''}
+                                        invalid={errors.Eventname !== ''}
+                                        onBlur={this.handleBlur('Eventname')}
                                         onChange={this.handleInputChange} />
+                                    <FormFeedback>{errors.Eventname}</FormFeedback>
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -56,13 +136,17 @@ class Contact extends Component {
                                     <Input type="textarea" id="Description" name="Description"
                                         rows="12"
                                         value={this.state.Description}
+                                        valid={errors.Description=== ''}
+                                        invalid={errors.Description !== ''}
+                                        onBlur={this.handleBlur('Description')}
                                         onChange={this.handleInputChange} />
+                                    <FormFeedback>{errors.Description}</FormFeedback>
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
                             <Label htmlFor="Eventlink" md={2}>Event Link</Label>
                                 <Col md={10}>
-                                    <Input type="link" id="Eventlink" name="link"
+                                    <Input type="text" id="Eventlink" name="Eventlink"
                                         placeholder="Meeting Link"
                                         value={this.state.Eventlink}
                                         onChange={this.handleInputChange} />
@@ -74,7 +158,34 @@ class Contact extends Component {
                                     <Input type="tel" id="telnum" name="telnum"
                                         placeholder="Tel. number"
                                         value={this.state.telnum}
+                                        valid={errors.Contact === ''}
+                                        invalid={errors.Contact !== ''}
+                                        onBlur={this.handleBlur('Contact')}
                                         onChange={this.handleInputChange} />
+                                    <FormFeedback>{errors.Contact}</FormFeedback>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                            <Label htmlFor="startDate" md={2}>Select Date</Label>
+                                <Col md={10}>
+                                    
+                                <DatePicker
+                                 selected={ this.state.startDate }
+                                onChange={ this.handleChange }
+                                showTimeSelect
+                                timeFormat="HH:mm"
+                                timeIntervals={30}
+                                timeCaption="time"
+                                dateFormat="MMMM d, yyyy h:mm aa"
+          />
+                             <button className="btn btn-primary">Show Date</button>
+                             </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Col md={{size: 10, offset: 2}}>
+                                    <Button type="submit" color="primary">
+                                        Submit
+                                    </Button>
                                 </Col>
                             </FormGroup>
                         </Form>
